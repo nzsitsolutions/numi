65175bcc1ffbb5ac16f9c21cde688f9e344ab975
Matías Aguirre <support@nzsitsolutions.com.ar>
2026-05-31 00:04:33 -0300

    supabase config

--- nodejs/src/controllers/userController.ts
+++ nodejs/src/controllers/userController.ts
@@ -1,17 +1,13 @@
 import { Router } from "express";
 import supabase from "../../lib/supabase.js";
-
 const router = Router();
 
 router.get("", async (req, res) => {
-    try {
-        const { data, error } = await supabase.from('auth.users').select("*");
-        console.log("data", data);
-        console.error("error", error);
-        res.json(data);
-    } catch (e) {
-        res.status(500).send('not working');
-    }
+    const { data, error } = await supabase.auth.admin.listUsers();
+
+    if(error) return res.status(500).json(error);
+
+    res.status(200).json(data.users);
 });
 
 export default router;
