79df04e1a64f69216c1a62339fbcc9dd321e6af5
Matías Aguirre <support@nzsitsolutions.com.ar>
2026-05-30 22:27:15 -0300

    setting up projec

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 config();
 
 const PORT = process.env.PORT;
@@ -9,6 +10,9 @@ app.listen(PORT, () => {
     console.log(`server initialized at http://localhost:${PORT}`);
 })
 
+app.use(cors({
+    origin: "http://localhost:4200"
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
