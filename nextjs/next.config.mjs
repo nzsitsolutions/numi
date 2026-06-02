ae9035bc5841c21cdcf66395b3f3237dd4bca03b
Matias Aguirre <support@nzsitsolutions.com.ar>
2026-06-01 22:15:02 -0300

    changes

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -5,15 +5,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*',
-      },
-    ]
-  },
+  }
 }
 
 export default nextConfig
